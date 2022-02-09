7520a50452801cb1a256d7fdef3f07dfa8b9f23a
Agata Figura <agata.figura1@gmail.com>
2022-02-09 09:18:31 +0100

    post working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+  <HelloWorld/>
+</div>
 </template>
 
 <style>
@@ -13,8 +9,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
@@ -30,3 +24,14 @@
   color: #42b983;
 }
 </style>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  
+  
+  components: {
+    HelloWorld
+  }
+}
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- form -->
+  <div>
+    <div class="w-full flex justify-center">
+      <form @submit.prevent="submit" class="p-10 flex flex-col w-full items-start">
+        <h1 class="font-extrabold">Add your post</h1>
+        <div class="flex flex-col gap-y-1 w-1/5">
+          <label>Your name</label>
+          <input type="text" ref="post_author" class="shadow border border-gray-700 rounded"/>
+        </div>
+        <div class="flex flex-col gap-y-1 w-1/5">
+          <label>Title</label>
+          <input type="text" ref="post_title" class="shadow border border-gray-700 rounded"/>
+        </div>
+        <div class="flex flex-col gap-y-1 w-1/2">
+          <label>Description</label>
+          <textarea ref="post_desc" class="shadow border border-gray-700 rounded p-2"/>
+        </div>
+          <button v-on:click="postData" class="bg-gray-700 text-white rounded p-1 w-100 place-self-start mt-2">Submit</button>
+      </form>
+    </div>
+
+
+    <!-- posts -->
+    <!-- <ul class="p-10 flex flex-col gap-y-7">
+      <li
+        v-for="post in posts"
+        :key="post.id"
+        class="flex flex-col gap-y-2"
+      >
+        <h2 class="font-extrabold">{{ data.title }}</h2>
+        <p>{{ data.description }}</p>
+        <div class="flex justify-end gap-x-2">
+          <h3>{{ data.author }},</h3>
+          <p>{{ data.publishedAt }}</p>
+        </div>
+      </li>
+    </ul> -->
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "App",
+  data() {
+    return {
+      postResult: null
+    }
+  },
+  methods: {
+    
+    async postData() {
+      const postData = {
+        id: 1,
+        title: this.$refs.post_title.value,
+        description: this.$refs.post_desc.value,
+        author: this.$refs.post_author.value,
+        publishedAt: "02-08-2022",
+      };
+      {
+        const res = await fetch('https://testdatabase-6dea.restdb.io/rest/blogs', {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+            "x-apikey": "9c6c5e34053fb83ca11826ba664d75b1a71e1"
+          },
+          body: JSON.stringify(postData)
+        });
+      
+        const data = await res.json();
+      } 
+      getData();
+    },
+    async getData() {
+      const response = await fetch('https://testdatabase-6dea.restdb.io/rest/blogs', {
+        method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            "x-apikey": "9c6c5e34053fb83ca11826ba664d75b1a71e1"
+          },
+      });
+      const data = await response.json();
+      this.totalVuePackages = data.total;
+      console.log(data.total);
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
